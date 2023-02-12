3878559756c1aa1cf87d315e5d0b78036b7544c5
JerryShao3234 <shaojerry@icloud.com>
2023-02-12 14:21:45 -0800

    Implement basic routes

--- backend/index.js
+++ backend/index.js
@@ -9,24 +9,80 @@ totalAccesses = 0
 app.use(express.json())
 
 app.get("/", (req, res) => {
-	res.send("Hello World! Total accesses since last server restart: " + totalAccesses)
+	res.send("Server is currently up! Total accesses since last server restart: " + totalAccesses)
 	totalAccesses += 1
 })
 
+app.post("/signup", (req, res) => {
+	console.log(req.body)
+	client.db("sdmsDB").collection("user").insertOne(
+		{
+			"username": req.body.username,
+			"password": req.body.password,
+			"de1socID": req.body.de1socID,
+			"visitHistory": {}
+		}
+	)
+})
+
+app.get("/gethistory", (req, res) => {
+	console.log(req.query)
+	client.db("sdmsDB").collection("user").findOne(
+		{
+			"username": req.query.username,
+		}
+	).then((result) => {
+		res.send(result.visitHistory)
+	})
+})
+
+app.post("/visit", (req, res) => {
+	console.log(req.body)
+	client.db("sdmsDB").collection("user").findOne(
+		{
+			"username": req.body.username,
+		}
+	).then((result) => {
+		//add visit to user's visit history
+		var visitHistory = result.visitHistory
+		visitHistory[req.body.visitID] = {
+			"date": req.body.date,
+			"time": req.body.time,
+			"intent": req.body.intent,
+			"visitor": req.body.visitor,
+			"message": req.body.message
+		}
+	})
+})
+
+/*
+ * Realtime notification from homeowner to visitor (needs DE1-SoC endpoint to be integrated)
+*/
+app.post("/messagevisitor", (req, res) => {
+	client.db("sdmsDB").collection("user").findOne(
+		{
+			"username": req.body.username,
+		}
+	).then((result) => {
+		//add message to user's visit history
+		console.log(req.body.message)
+	})
+})
+
 async function run() {
-	    try {
-		            await client.connect() //connect to client (db), we are app
-		            console.log("Successfully connected to the database")
-		            var server = app.listen(3000, (req, res) => { //init server after connecting to db
-				                var host = server.address().address
-				                var port = server.address().port
-				                console.log("Example server successfully running at http://%s:%s", host, port)
-				            })
-		        }
-	    catch(err) {
-		            console.log(err)
-		            await client.close()
-		        }
+	try {
+		await client.connect() //connect to client (db), we are app
+		console.log("Successfully connected to the database")
+		var server = app.listen(3000, (req, res) => { //init server after connecting to db
+			var host = server.address().address
+			var port = server.address().port
+			console.log("Example server successfully running at http://%s:%s", host, port)
+		})
+	}
+	catch (err) {
+		console.log(err)
+		await client.close()
+	}
 }
 
 run()
